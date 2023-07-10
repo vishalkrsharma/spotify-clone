ff7b58a719da2d897239104c27ee6af5dcae037c
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-07-10 18:31:14 +0530

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+
+const font = Figtree({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Dive into Music!',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
