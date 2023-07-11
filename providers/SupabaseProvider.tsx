22ace394391770db8ad31d27cc90410b73494386
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-07-11 16:31:41 +0530

    auth providers done

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ReactNode, FC, useState } from 'react';
+import { Database } from '@/types_db';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+interface SupabaseProviderProps {
+  children: ReactNode;
+}
+
+const SupabaseProvider: FC<SupabaseProviderProps> = ({ children }) => {
+  const [supabaseClient] = useState(() => createClientComponentClient<Database>());
+
+  return <SessionContextProvider supabaseClient={supabaseClient}>{children}</SessionContextProvider>;
+};
+
+export default SupabaseProvider;
---
+++ providers/UserProvider.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { MyUserContextProvider } from '@/hooks/useUser';
+import { FC, ReactNode } from 'react';
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+const UserProvider: FC<UserProviderProps> = ({ children }) => {
+  return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
+
+export default UserProvider;
